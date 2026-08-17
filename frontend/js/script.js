65dbee1709398993ff4f29e726c83204f2983878
ganavirayee2 <ganavirayee2@gmail.com>
2026-08-17 18:36:22 +0530

    Remove scroll-to-top button from home page

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -624,17 +624,6 @@ function initNavigation() {
     });
   }
 
-  // Back to Top Button
-  const backToTopBtn = document.getElementById("back-to-top");
-  if (backToTopBtn) {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 400) backToTopBtn.classList.add("visible");
-      else backToTopBtn.classList.remove("visible");
-    });
-    backToTopBtn.addEventListener("click", () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-  }
 }
 
 // Global Document Initialization
